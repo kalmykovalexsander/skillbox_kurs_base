7a23110181831e85c706564fefcb0ad9a3d9dcd7
kalmykovalexsander <kalmykov_ao@mail.ru>
2022-05-17 14:36:13 +0300

    Lesson 7 complite

--- lesson_007/01_snowfall.py
+++ lesson_007/01_snowfall.py
@@ -2,42 +2,71 @@
 
 import simple_draw as sd
 
+
 # Шаг 1: Реализовать падение снежинки через класс. Внести в методы:
 #  - создание снежинки с нужными параметрами
 #  - отработку изменений координат
 #  - отрисовку
 
+sd.resolution = (1200, 800)
+snowflakes = {}
+tick = 0
+
 
 class Snowflake:
-    pass
+    size = {'min': 5, 'max': 20}
 
-    #
+    def __init__(self):
+        self.length = sd.random_number(Snowflake.size['min'], Snowflake.size['max'])
+        self.x = sd.random_number(0, sd.resolution[0])
+        self.y = sd.randint(sd.resolution[1] - 100, sd.resolution[1] + 100)
+        self.factor_a = sd.random_number(1, 10) / 10
+        self.factor_b = sd.random_number(1, 10) / 10
+        self.factor_c = sd.random_number(1, 120)
+
+    def draw(self, color=sd.COLOR_WHITE):
+        start_point = sd.get_point(x=self.x, y=self.y)
+        sd.snowflake(center=start_point,
+                     length=self.length,
+                     color=color,
+                     factor_a=self.factor_a,
+                     factor_b=self.factor_b,
+                     factor_c=self.factor_c)
+
+    def hide(self, color=sd.background_color):
+        self.draw(color)
+
+    def move(self):
+        self.x += sd.random_number(0, 2)
+        self.y -= Snowflake.size['max'] + 1 - self.length
 
 
-flake = Snowflake()
+def run_snowfall(snowflakes_count=0):
+    """Запуск снегопада"""
+    if len(snowflakes) != snowflakes_count:
+        snowflakes[len(snowflakes)] = Snowflake()
 
-while True:
-    flake.clear_previous_picture()
-    flake.move()
-    flake.draw()
-    if not flake.can_fall():
-        break
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
+    for num, snowflake in snowflakes.items():
+        snowflake.hide()
+        snowflake.move()
+        snowflake.draw()
 
-# шаг 2: создать снегопад - список объектов Снежинка в отдельном списке, обработку примерно так:
-# flakes = get_flakes(count=N)  # создать список снежинок
-# while True:
-#     for flake in flakes:
-#         flake.clear_previous_picture()
-#         flake.move()
-#         flake.draw()
-#     fallen_flakes = get_fallen_flakes()  # подчитать сколько снежинок уже упало
-#     if fallen_flakes:
-#         append_flakes(count=fallen_flakes)  # добавить еще сверху
-#     sd.sleep(0.1)
-#     if sd.user_want_exit():
-#         break
+        if snowflake.y < 0:
+            snowflakes[num] = Snowflake()
 
-sd.pause()
+
+if __name__ == '__main__':
+    while True:
+        tick += 1
+        sd.start_drawing()
+
+        if tick < 50:
+            run_snowfall(snowflakes_count=20)
+        elif tick > 50:
+            run_snowfall(snowflakes_count=50)
+
+        sd.sleep(0.05)
+        sd.finish_drawing()
+
+        if sd.user_want_exit():
+            break
--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,212 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-#
+
+class Water:
+
+    def __init__(self):
+        self.name = 'Water'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            substance = Storm()
+        elif isinstance(other, Fire):
+            substance = Steam()
+        elif isinstance(other, Earth):
+            substance = Dirt()
+        elif isinstance(other, Iron):
+            substance = Rust()
+        else:
+            substance = None
+
+        return substance
+
+
+class Fire:
+
+    def __init__(self):
+        self.name = 'Fire'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if isinstance(other, Air):
+            substance = Lightning()
+        elif isinstance(other, Water):
+            substance = Steam()
+        elif isinstance(other, Earth):
+            substance = Lava()
+        elif isinstance(other, Iron):
+            substance = Ingot()
+        else:
+            substance = None
+
+        return substance
+
+
+class Air:
+
+    def __init__(self):
+        self.name = 'Air'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            substance = Dust()
+        elif isinstance(other, Fire):
+            substance = Lightning()
+        elif isinstance(other, Earth):
+            substance = Dust()
+        else:
+            substance = None
+
+        return substance
+
+
+class Earth:
+    def __init__(self):
+        self.name = 'Earth'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            substance = Dirt()
+        elif isinstance(other, Fire):
+            substance = Lava()
+        elif isinstance(other, Air):
+            substance = Dust()
+        elif other == Iron():
+            substance = Ore()
+        else:
+            substance = None
+
+        return substance
+
+
+class Steam:
+    name = 'Steam'
+
+    def __str__(self):
+        return self.name
+
+
+class Dirt:
+    name = 'Dirt'
+
+    def __str__(self):
+        return self.name
+
+
+class Storm:
+    name = 'Storm'
+
+    def __str__(self):
+        return self.name
+
+
+class Lightning:
+    name = 'Lightning'
+
+    def __str__(self):
+        return self.name
+
+
+class Dust:
+    name = 'Dust'
+
+    def __str__(self):
+        return self.name
+
+
+class Lava:
+    name = 'Lava'
+
+    def __str__(self):
+        return self.name
+
+
+class Iron:
+    def __init__(self):
+        self.name = 'Iron'
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            substance = Rust()
+        elif isinstance(other, Fire):
+            substance = Ingot()
+        elif isinstance(other, Earth):
+            substance = Ore()
+        else:
+            substance = None
+
+        return substance
+
+
+class Rust:
+    """ржавчина"""
+    name = 'Rust'
+
+    def __str__(self):
+        return self.name
+
+
+class Ingot:
+    """слиток"""
+    name = 'Ingot'
+
+    def __str__(self):
+        return self.name
+
+
+class Ore:
+    """руда"""
+    name = 'Ore'
+
+    def __str__(self):
+        return self.name
+
+
+print(Water(), '+', Air(), '=', Water() + Air())
+print(Water(), '+', Fire(), '=', Water() + Fire())
+print(Water(), '+', Earth(), '=', Water() + Earth())
+print(Air(), '+', Fire(), '=', Air() + Fire())
+print(Air(), '+', Earth(), '=', Air() + Earth())
+print(Fire(), '+', Earth(), '=', Fire() + Earth())
+
+print('=====')
+print(Fire(), '+', Dust(), '=', Fire() + Dust())
+print(Earth(), '+', Fire(), '=', Earth() + Fire())
+
+print('=====')
+print(Iron(), '+', Fire(), '=', Iron() + Fire())
+print(Iron(), '+', Water(), '=', Iron() + Water())
+print(Iron(), '+', Earth(), '=', Iron() + Earth())
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
